04288f7be1d819bff724441b0be18b473d2e3afc
christian jardinel <ascenciojardinel1@aol.com>
2022-04-15 19:03:19 -0700

    added django forum

---
+++ static/js/posts.js
@@ -0,0 +1,10 @@
+/////////////////////////////
+// JavaScript for Posts page
+/////////////////////////////
+
+$(function(){
+    // Executed when js-menu-icon js clicked
+    $('.js-menu-icon').click(function() {
+        $(this).next().toggle();
+    })
+})
